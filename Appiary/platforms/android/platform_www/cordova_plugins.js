f35a25e0d1a090c3f48d1ae1a0884c54b34387aa
arodang <sfbattlefield2@gmail.com>
2016-09-11 15:09:25 -0400

    Added SQL and InAppBrowser plugins

--- Appiary/platforms/android/platform_www/cordova_plugins.js
+++ Appiary/platforms/android/platform_www/cordova_plugins.js
@@ -28,6 +28,23 @@ module.exports = [
             "cordova.plugins.Keyboard"
         ],
         "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
+        "id": "cordova-sqlite-storage.SQLitePlugin",
+        "pluginId": "cordova-sqlite-storage",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -38,7 +55,9 @@ module.exports.metadata =
     "cordova-plugin-whitelist": "1.2.2",
     "cordova-plugin-splashscreen": "3.2.2",
     "cordova-plugin-statusbar": "2.1.3",
-    "ionic-plugin-keyboard": "2.2.1"
-};
+    "ionic-plugin-keyboard": "2.2.1",
+    "cordova-plugin-inappbrowser": "1.4.0",
+    "cordova-sqlite-storage": "1.4.7"
+}
 // BOTTOM OF METADATA
 });
