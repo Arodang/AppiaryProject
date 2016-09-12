03e3dc29bd86c7169493e0af995f05297ade1320
arodang <sfbattlefield2@gmail.com>
2016-09-11 22:12:11 -0400

    Added login page, removed old starter app code

--- Appiary/www/js/common/constants.js
+++ Appiary/www/js/common/constants.js
@@ -1,6 +1,11 @@
 ﻿angular.module('apiary.common', [])
-    .constant('googleLogin', {
-        'clientId': '1062484540187-9vkejq16ec2ladmu7cn0gk0cesj7dfla.apps.googleusercontent.com',
-        'clientSecret': 'iV3LwUqsaXlfddN5JbM_c7U5',
-        'redirectURL': 'http://localhost/appiary/www'
+    .constant('LoginAuths', {
+        'google': {
+            'clientId': '1062484540187-9vkejq16ec2ladmu7cn0gk0cesj7dfla.apps.googleusercontent.com',
+            'clientSecret': 'iV3LwUqsaXlfddN5JbM_c7U5'
+        },
+        'facebook': {
+            'clientId': '159318631179311',
+            'clientSecret': '982a3518aae3df5ecb6eebb8ccaf0514'
+        }
     });
