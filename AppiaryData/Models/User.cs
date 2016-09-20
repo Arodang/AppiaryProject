6fedea940a317889dcdf59c84e1590956911d732
arodang <sfbattlefield2@gmail.com>
2016-09-20 18:02:52 -0400

    Added User data model, unimplemented UsersController for API

---
+++ AppiaryData/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppiaryData.Models
+{
+    public class User : BaseModel
+    {
+        [Key, Required]
+        public int UserId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string ProfileId { get; set; }
+
+        public List<Apiary> Apiaries { get; set; }
+    }
+}
