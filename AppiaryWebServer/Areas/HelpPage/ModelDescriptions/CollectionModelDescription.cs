ba139287e34c906023468590d7a28e648eda308a
arodang <sfbattlefield2@gmail.com>
2016-11-29 11:21:08 -0500

    Added AppiaryWebServer project

---
+++ AppiaryWebServer/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace AppiaryWebServer.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
